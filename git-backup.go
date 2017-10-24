e37d99b4e5bc1fed929312a6c10d4d0749d0be47
Kirill Smelkov <kirr@nexedi.com>
2017-10-24 21:03:23 +0300

    Relicense to GPLv3+ with wide exception for all Free Software / Open Source projects + Business options.
    Nexedi stack is licensed under Free Software licenses with various exceptions
    that cover three business cases:
    
    - Free Software
    - Proprietary Software
    - Rebranding
    
    As long as one intends to develop Free Software based on Nexedi stack, no
    license cost is involved. Developing proprietary software based on Nexedi stack
    may require a proprietary exception license. Rebranding Nexedi stack is
    prohibited unless rebranding license is acquired.
    
    Through this licensing approach, Nexedi expects to encourage Free Software
    development without restrictions and at the same time create a framework for
    proprietary software to contribute to the long term sustainability of the
    Nexedi stack.
    
    Please see https://www.nexedi.com/licensing for details, rationale and options.

--- git-backup.go
+++ git-backup.go
@@ -5,10 +5,17 @@
 // it under the terms of the GNU General Public License version 3, or (at your
 // option) any later version, as published by the Free Software Foundation.
 //
+// You can also Link and Combine this program with other software covered by
+// the terms of any of the Free Software licenses or any of the Open Source
+// Initiative approved licenses and Convey the resulting work. Corresponding
+// source of such a combination shall include the source code for all other
+// software used.
+//
 // This program is distributed WITHOUT ANY WARRANTY; without even the implied
 // warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 // See COPYING file for full licensing terms.
+// See https://www.nexedi.com/licensing for rationale and options.
 
 /*
 Git-backup - Backup set of Git repositories & just files; efficiently
